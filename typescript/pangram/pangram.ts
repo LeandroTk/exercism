b5b1042bd5c814986296e47dc797a4cdedabf760
= <leandrotk100@gmail.com>
2021-01-03 10:08:06 -0300

    Improve pangram

--- typescript/pangram/pangram.ts
+++ typescript/pangram/pangram.ts
@@ -1,10 +1,5 @@
 export default class Pangram {
   private phrase: string;
-  private alphabet = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
-    'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-    's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-  ];
 
   constructor(phrase: string) {
     this.phrase = phrase;
@@ -12,18 +7,12 @@ export default class Pangram {
 
   isPangram() {
     const phrase = this.cleanPhrase();
-    let isPangram = true;
-
-    this.alphabet.forEach((letter) => {
-      if (!phrase.includes(letter)) isPangram = false;
-    });
-
-    return isPangram;
+    const phraseSet = new Set(phrase);
+    return phraseSet.size === 26;
   }
 
   private cleanPhrase = () =>
     this.phrase
-      .match(/[a-zA-Z]+/g)
-      ?.join('')
-      ?.toLowerCase() || '';
+      .replace(/[^a-zA-Z]/g, '')
+      .toLowerCase();
 }
